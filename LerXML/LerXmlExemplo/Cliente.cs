1ab2e787616251ac1c39864d76507460b595cd57
itamar.pedreira <itasouza@yahoo.com.br>
2019-10-07 11:34:37 -0300

    Fazer a leitura e criação de XML

---
+++ LerXML/LerXmlExemplo/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace LerXmlExemplo
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+        public bool Exportar(string caminho)
+        {
+            try
+            {
+                FileStream stream = new FileStream(caminho, FileMode.Create);
+                XmlSerializer serializador = new XmlSerializer(typeof(Cliente));
+                serializador.Serialize(stream, this);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static Cliente Importar(string caminho)
+        {
+            try
+            {
+                FileStream stream = new FileStream(caminho, FileMode.Open);
+                XmlSerializer desserializador = new XmlSerializer(typeof(Cliente));
+                Cliente cliente = (Cliente)desserializador.Deserialize(stream);
+                return cliente;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ LerXML/LerXmlExemplo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LerXmlExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Escolha uma opção (digite o número correspondente):");
+                Console.WriteLine("1 - CADASTRAR CLIENTE 2 - IMPORTAR CADASTRO; 0 - SAIR");
+                Console.WriteLine("--------------------");
+                string opc = Console.ReadLine();
+                switch (opc)
+                {
+                    case "1":
+                        Cliente novoCliente = new Cliente();
+                        Console.Write("Nome: ");
+                        novoCliente.Nome = Console.ReadLine();
+                        Console.Write("Email: ");
+                        novoCliente.Email = Console.ReadLine();
+                        Console.WriteLine("Informe o caminho do arquivo para exportar o cadastro do cliente: ");
+        
+                        string caminhoExportacao = Console.ReadLine();
+                        if (novoCliente.Exportar(caminhoExportacao))
+                            Console.WriteLine("Cadastro exportado com sucesso.");
+                        break;
+                    case "2":
+                        Console.WriteLine("Informe o caminho do arquivo para importar o cadastro do cliente: ");
+        
+                        string caminhoImportacao = Console.ReadLine();
+                        Cliente cliente = Cliente.Importar(caminhoImportacao);
+                        if (cliente != null)
+                        {
+                            Console.WriteLine("Nome: " + cliente.Nome);
+                            Console.WriteLine("Email: " + cliente.Email);
+                        }
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+
+    }
+}
