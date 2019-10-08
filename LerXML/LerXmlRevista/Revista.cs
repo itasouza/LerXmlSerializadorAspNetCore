3611562f36dca1e5bd3452e8fdd4436c70c381ca
itasouza <itasouza@yahoo.com.br>
2019-10-07 21:12:23 -0300

    Adicionado novo arquivo

---
+++ LerXML/LerXmlRevista/Revista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace LerXmlRevista
+{
+
+    [XmlRoot("revista")]
+    public class Revista
+    {
+
+    }
+}
+//https://www.youtube.com/watch?v=UvEF7UPh1Qg
