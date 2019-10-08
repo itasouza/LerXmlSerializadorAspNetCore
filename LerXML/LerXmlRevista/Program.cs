34aa9ef8d5f62b45af3219673f93399da28b6a98
itasouza <itasouza@yahoo.com.br>
2019-10-07 23:40:02 -0300

    Ajuste para leitura de revista

--- LerXML/LerXmlRevista/Program.cs
+++ LerXML/LerXmlRevista/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace LerXmlRevista
 {
@@ -6,6 +8,21 @@ namespace LerXmlRevista
     {
         static void Main(string[] args)
         {
+
+
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Revista));
+                StreamReader sr = new StreamReader(@"C:\GitHub\LerXmlSerializadorAspNetCore-master\LerXmlSerializadorAspNetCore-master\LerXML\LerXmlExemplo\2323.xml");
+                //var revista = xmlSerializer.Deserialize(sr);
+                Revista revista1 = (Revista)xmlSerializer.Deserialize(sr);
+                Console.WriteLine("Deu");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
